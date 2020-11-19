141f1a46c6a548e16406714c4ff9f6ab89a870f5
flinn97 <taylormdavidson@gmail.com>
2020-11-19 16:59:37 -0700

    all

--- back-end/server.js
+++ back-end/server.js
@@ -1,17 +1,94 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const multer = require('multer');
+const mongoose = require('mongoose');
 
+
+mongoose.connect('mongodb://localhost:27017/museum', {
+	useUnifiedTopology: true,
+	  useNewUrlParser: true
+});
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+	  extended: false
 }));
+const upload = multer({
+	  dest: '../front-end/public/images/',
+	  limits: {
+		      fileSize: 10000000
+		    }
+});
 
-const mongoose = require('mongoose');
 
-// connect to the database
-mongoose.connect('mongodb://localhost:27017/museum', {
-  useNewUrlParser: true
+const itemSchema = new mongoose.Schema({
+	  title: String,
+	  path: String,
+	description: String,
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+app.post('/api/photos', upload.single('photo'), async (req, res) => {
+	if (!req.file) {
+		    return res.sendStatus(400);
+		  }
+	  res.send({
+		      path: "/images/" + req.file.filename
+		    });
+});
+
+app.post('/api/items', async (req, res) => {
+	  const item = new Item({
+		      title: req.body.title,
+		      path: req.body.path,
+		  description: req.body.description,
+		    });
+	  try {
+		      await item.save();
+		  console.log(item.id);
+		      res.send(item);
+		    } catch (error) {
+			        console.log(error);
+			        res.sendStatus(500);
+			      }
+});
+
+app.get('/api/items', async (req, res) => {
+	  try {
+		      let items = await Item.find();
+		      res.send(items);
+		    } catch (error) {
+			        console.log(error);
+			        res.sendStatus(500);
+			      }
+});
+
+app.delete('/api/items/:id', async (req, res) => {
+	try {
+		await Item.deleteOne({_id: req.params.id});
+		res.sendStatus(200);
+	}
+	catch (error) {
+		console.log(error);
+		res.sendStatus(500);
+	}
+});
+app.put('/api/items/:id', async (req, res) => {
+	try {
+		let item = await Item.findOne({
+			_id: req.params.id
+		});
+		item.title= req.body.title;
+		item.description=req.body.description;
+		await item.save();
+		res.send(item);
+
+	}
+	catch (error) {
+		console.log(error);
+		res.sendStatus(500);
+	}
 });
 
 
